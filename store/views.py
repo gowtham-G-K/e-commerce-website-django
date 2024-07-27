ac471b3b5750aff21c69560b640b6c8b871d7174
gowtham-G-K <gowthamgk24112002@gmail.com>
2024-07-27 22:25:33 +0530

    This is my first commit

---
+++ store/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from category.models import Category
+
+# Create your views here.
+def store(request, category_slug=None):
+    categories = None
+    products = Product.objects.all().filter(is_available=True)
+    products_count = products.count()
+    if category_slug:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=categories, is_available=True)
+        products_count = products.count()
+    context = {
+        "products": products,
+        "products_count": products_count,
+    }
+    return render(request, 'store/store.html', context)
+
+def product_detail(request, category_slug=None, product_slug=None):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise ValueError(str(e))
+    context = {
+        'single_product': single_product
+    }
+    return render(request, 'store/product_detail.html', context)
